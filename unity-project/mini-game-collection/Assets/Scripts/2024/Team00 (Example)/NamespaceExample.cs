ec8906c1f7a6a07f3b87a566c812ecfd2f00650c
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-09-14 13:41:50 -0400

    Add example script to show how namespaces should be configured

---
+++ unity-project/mini-game-collection/Assets/Scripts/2024/Team00 (Example)/NamespaceExample.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// You MUST place you code in a proper namespace.
+// Use the same first two namespaces, then append with .Team## where ## is you 2-digit team number.
+// All code your team creates has to be in that namespace. Pull-requests will be rejected if not.
+namespace MiniGameCollection.Games2024.Team00
+{
+    public class NamespaceExample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
